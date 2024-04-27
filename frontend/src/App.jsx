57025ed2756310f67f4fe18a891c28d00931b284
Lucas Abello <124079018+lexO-dat@users.noreply.github.com>
2024-04-26 20:12:44 -0400

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:5172');
+const socket = io('https://chat-api-w1i0.onrender.com/');
 
 function App() {
   const [message, setMessage] = useState('');
